eb1c0ba2af4963a0315a02de26f3cfe25a57bd56
yakushev85 <hrip85@gmail.com>
2024-04-07 08:28:26 +0300

    use tween for movement

--- app/Items/MessageHint.gd
+++ app/Items/MessageHint.gd
@@ -1,6 +1,5 @@
 extends Node2D
 
-export var y_speed = 100
 export var y_anim = 200
 var is_moving_down = true
 
@@ -21,33 +20,41 @@ func set_orientation_drop(v):
 func set_damage_message(value):
 	$DamageLabel.text = value
 	$DamageLabel.show()
+	do_movement()
 	
 
 func set_heal_message(value):
 	$HealLabel.text = value
 	$HealLabel.show()
+	do_movement()
 
 func set_reward_message(value):
 	$RewardLabel.text = value
 	$RewardLabel.show()
+	do_movement()
 
-func _process(delta):
+func do_movement():
 	if $DamageLabel.is_visible_in_tree():
-		animate_move($DamageLabel, y_speed*delta, is_moving_down)
+		animate_move($DamageLabel, is_moving_down)
 	elif $HealLabel.is_visible_in_tree():
-		animate_move($HealLabel, y_speed*delta, is_moving_down)
+		animate_move($HealLabel, is_moving_down)
 	elif $RewardLabel.is_visible_in_tree():
-		animate_move($RewardLabel, y_speed*delta, is_moving_down)
+		animate_move($RewardLabel, is_moving_down)
 
 
-func animate_move(a_label:Label, delta_y, is_down=true):
+func animate_move(a_label:Label, is_down=true):
+	var new_position = a_label.rect_position
+	
 	if is_down:
-		a_label.rect_position.y += delta_y
-		
-		if a_label.rect_position.y > y_anim:
-			queue_free()
+		new_position.y = new_position.y + y_anim
 	else:
-		a_label.rect_position.y -= delta_y
-		
-		if a_label.rect_position.y < -1*y_anim:
-			queue_free()
+		new_position.y = new_position.y - y_anim
+	
+	$Tween.interpolate_property(a_label, "rect_position", 
+		a_label.rect_position, new_position, 1, 
+		Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	$Tween.start()
+
+
+func _on_Tween_tween_all_completed():
+	queue_free()
--- app/Node2DMv.gd
+++ app/Node2DMv.gd
@@ -2,9 +2,6 @@ extends Node2D
 
 class_name Node2DMv
 
-export var speed = 500
-
-var move_position = Vector2.ZERO
 var is_moving = false
 var is_remove_on_stopping = false
 
@@ -12,20 +9,20 @@ var is_remove_on_stopping = false
 func move_to(new_position, remove_on_finish = false):
 	is_moving = true
 	is_remove_on_stopping = remove_on_finish
-	move_position = new_position
+	
+	var tween = get_node("Tween") as Tween
+	tween.interpolate_property(
+		self, "position", 
+		self.position, new_position, 1, 
+		Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	tween.start()
 
 
 func _process(delta):
-	if is_moving:
-		var velocity = move_position - position
-		var n_velocity = velocity.normalized()*speed*delta
-		
-		if velocity.length() > n_velocity.length():
-			position = position + n_velocity
-		else:
-			position = move_position
-			is_moving = false
-			
-			if is_remove_on_stopping:
-				queue_free()
+	var tween = get_node("Tween") as Tween
+	
+	if is_moving and not tween.is_active():
+		is_moving = false
+		if is_remove_on_stopping:
+			queue_free()
 
