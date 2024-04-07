1dc8dad156d32a5b3bb152cb934b1a5fb2d8bd64
yakushev85 <hrip85@gmail.com>
2024-04-07 16:39:13 +0300

    refactor movement logic

--- app/Node2DMv.gd
+++ app/Node2DMv.gd
@@ -2,27 +2,17 @@ extends Node2D
 
 class_name Node2DMv
 
-var is_moving = false
-var is_remove_on_stopping = false
 
-
-func move_to(new_position, remove_on_finish = false):
-	is_moving = true
-	is_remove_on_stopping = remove_on_finish
-	
+func move_to(new_position, remove_on_finish = false):	
 	var tween = get_node("Tween") as Tween
+	
 	tween.interpolate_property(
 		self, "position", 
 		self.position, new_position, 1, 
 		Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+		
+	if remove_on_finish:
+		tween.interpolate_callback(self, 1, "queue_free")
+		
 	tween.start()
-
-
-func _process(delta):
-	var tween = get_node("Tween") as Tween
-	
-	if is_moving and not tween.is_active():
-		is_moving = false
-		if is_remove_on_stopping:
-			queue_free()
 
